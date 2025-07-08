a6923cfddd82cd2ee4fbc14a55db9bb683eb8777
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2025-07-08 17:04:11 +0200

    added rotating file handler logger opt + elastic integration through customized external lib logger

--- helpers/logging.py
+++ helpers/logging.py
@@ -1,6 +1,10 @@
 import logging
 import os
+
 import sys
+from logging.handlers import RotatingFileHandler
+
+from helpers.elastic import CustomElasticHandler
 
 BASE_DIR: str = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 DEFAULT_LOGGING_FORMAT: str = "%(asctime)s [%(levelname)s] %(name)s -- %(message)s"
@@ -24,19 +28,38 @@ def configure_worker_logger(handler: logging.StreamHandler or logging.Handler, c
 
 
 def configure_pacer_logger(config: dict, logger: logging.Logger) -> list:
-    log_level: str = config.get("logging").get("logLevel", "INFO")
-    print_format: str = config.get("logging").get("printFormat", DEFAULT_LOGGING_FORMAT)
+    logging_config: dict = config.get("logging")
+
+    log_level: str = logging_config.get("logLevel", "INFO")
+    print_format: str = logging_config.get("printFormat", DEFAULT_LOGGING_FORMAT)
 
     handlers: list = []
 
-    if config.get("logging").get("file").get("enabled", False):
-        log_file: str = config.get("logging").get("file").get("path",
-                                                              os.path.join(BASE_DIR, "logs", "icat-pacer.log"))
+    file_logging_config: dict = logging_config.get("file")
+
+    if file_logging_config.get("enabled", False):
+        log_file: str = file_logging_config.get("path", os.path.join(BASE_DIR, "logs", "icat-pacer.log"))
         os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
-        handlers.append(logging.FileHandler(log_file))
+        if file_logging_config.get("rotate", False):
+            max_bytes: int = file_logging_config.get("maxMBytes", 100 * 1024)
+            backup_count: int = file_logging_config.get("backupCount", 5)
+            handlers.append(RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count))
+        else:
+            handlers.append(logging.FileHandler(log_file))
 
-    if config.get("logging").get("console").get("enabled", False):
+    elastic_logging_config: dict = logging_config.get("elastic")
+    if elastic_logging_config.get("enabled", False):
+        server_url: str = elastic_logging_config.get("serverUrl", "")
+        index: str = elastic_logging_config.get("indexName")
+        service_name: str = elastic_logging_config.get("serviceName", "icat-pacer")
+        service_environment: str = elastic_logging_config.get("serviceEnvironment", "local")
+        logging.getLogger('elastic_transport.transport').setLevel(logging.ERROR)
+
+        handlers.append(CustomElasticHandler(host=server_url, index=index, service_name=service_name,
+                                             service_environment=service_environment))
+
+    if logging_config.get("console").get("enabled", False):
         handlers.append(logging.StreamHandler(sys.stdout))
 
     logger.setLevel(log_level)
