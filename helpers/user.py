346e1cddfce72eefbc2dabb1ffc42ffcebef32b6
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2025-07-09 13:02:21 +0200

    fixed fallback value usage

--- helpers/user.py
+++ helpers/user.py
@@ -40,20 +40,20 @@ def create_user_context(user_data: str or dict, username_prefix: str = "") -> Us
     return UserContext(
         first_name=user_dict.get("first_name"),
         last_name=user_dict.get("last_name"),
-        orcid=user_dict.get("ORCID", ""),
+        orcid=user_dict.get("ORCID"),
         email=user_dict.get("email"),
         is_staff=user_dict.get("is_staff", False),
         enabled=user_dict.get("enabled", False),
         uos_id=user_dict.get("id"),
         usernames=[f"{username_prefix}{i.get("username")}" for i in user_dict.get("user_list", [])],
         affiliation=Affiliation(
-            id=aff.get("id", 0),
-            name=aff.get("name", ""),
-            code=aff.get("code", ""),
-            department_name=aff.get("department_name", ""),
-            department_code=aff.get("department_code", ""),
-            unit=aff.get("unit", ""),
-            city=aff.get("city", ""),
+            id=aff.get("id") or 0,
+            name=aff.get("name") or "",
+            code=aff.get("code") or "",
+            department_name=aff.get("department_name") or "",
+            department_code=aff.get("department_code") or "",
+            unit=aff.get("unit") or "",
+            city=aff.get("city") or "",
             country_code=aff.get("country_code", ""),
         )
     )
