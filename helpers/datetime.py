de735ee356184eca7e0919cae7c69528338d5ea1
marmenter <marmenter@cells.es>
2025-08-06 12:35:12 +0200

    MPM-3901/feature - proposal sync

--- helpers/datetime.py
+++ helpers/datetime.py
@@ -1,21 +1,12 @@
 from datetime import datetime
 
-# ISO 8601 with milliseconds and 'Z' for UTC
-DATETIME_ISO_UTC_MS = "%Y-%m-%dT%H:%M:%S.%fZ"
-
-# ISO 8601 without milliseconds
+# Datetime formats
 DATETIME_ISO = "%Y-%m-%dT%H:%M:%S"
-
-# ISO 8601 with space separator instead of 'T'
+DATETIME_ISO_8601_UTC = '%Y-%m-%dT%H:%M:%SZ'
+DATETIME_ISO_UTC_MS = "%Y-%m-%dT%H:%M:%S.%fZ"
 DATETIME_ISO_SPACE = "%Y-%m-%d %H:%M:%S"
-
-# ISO 8601 with milliseconds and space separator
 DATETIME_ISO_SPACE_MS = "%Y-%m-%d %H:%M:%S.%f"
-
-# Common US format (month/day/year)
 DATETIME_US = "%m/%d/%Y %H:%M:%S"
-
-# Common EU format (day/month/year)
 DATETIME_EU = "%d/%m/%Y %H:%M:%S"
 
 # Date-only formats
@@ -25,10 +16,6 @@ DATE_EU = "%d/%m/%Y"
 
 
 def try_parse_datetime(date_str: str) -> datetime:
-    """
-    Attempts to parse a date string using multiple known datetime formats.
-    Returns a datetime object if parsing succeeds, otherwise raises ValueError.
-    """
     datetime_formats = [
         DATETIME_ISO_UTC_MS,
         DATETIME_ISO,
