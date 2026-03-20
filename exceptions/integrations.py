d4dff0e88eadb74614258beee9c58b92a6316527
Rodrigo Cabezas Quirós <rcabezas@cells.es>
2026-03-20 14:09:13 +0100

    added icatplus client from MPM-3898 & changed to custom excp in clients

---
+++ exceptions/integrations.py
@@ -0,0 +1,11 @@
+from exceptions.base import PACERError
+
+
+class PaNOSCClientError(PACERError):
+    pass
+
+class DataciteClientError(PACERError):
+    pass
+
+class ICATPlusClientError(PACERError):
+    pass
